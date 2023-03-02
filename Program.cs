289224c58462cb64c1dde6d784ae47b7d180d598
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-03-02 17:01:24 +0530

    Enter Valid First Name

--- Program.cs
+++ Program.cs
@@ -5,6 +5,24 @@
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
+
+          FirstName firstname = new FirstName();
+
+            Console.Write("Enter Your First Name : ");
+
+            string FirstName = Console.ReadLine();
+
+            bool val = firstname.ValidateFirstName(FirstName);
+
+            if (val)
+            {
+                Console.WriteLine("FirstName is Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid FirstName");
+            }
         }
+
     }
 }
